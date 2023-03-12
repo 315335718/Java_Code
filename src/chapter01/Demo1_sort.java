dbc83af58febdcea4e0ecdf431e5d70b5a02203f
Accumulate <315335718@qq.com>
2023-03-12 16:57:35 +0800

    Default Changelist

--- src/chapter01/Demo1_sort.java
+++ src/chapter01/Demo1_sort.java
@@ -3,54 +3,64 @@ package chapter01;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-
-public class Demo1_sort {
-
-    public static void bubble_sort(int a[]){
+class Sort{
+    String type;
+    int[] a;
+    void bubble_sort(int[] a){
+        this.a = a.clone();
         System.out.println("#########   冒泡排序   #########");
-        System.out.println(Arrays.toString(a));
-        for (int i = a.length - 1; i > 0; i--) {
+        System.out.println(Arrays.toString(this.a));
+        for (int i = this.a.length - 1; i > 0; i--) {
             boolean flag = true;
             for (int j = 0; j < i; j++) {
-                if(a[j]>a[j+1]){
-                    int temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
+                if(this.a[j]>this.a[j+1]){
+                    int temp = this.a[j];
+                    this.a[j] = this.a[j+1];
+                    this.a[j+1] = temp;
                     flag = false;
                 }
             }
             if (flag){
                 break;
             }
-            System.out.println(Arrays.toString(a));
+            System.out.println(Arrays.toString(this.a));
         }
     }
-
-
-    public static void select_sort(int a[]){
+    void select_sort(int[] a){
+        this.a = a.clone();
         System.out.println("\n#########   选择排序   #########");
-        System.out.println(Arrays.toString(a));
-        for (int i = 0; i < a.length-1; i++) {
+        System.out.println(Arrays.toString(this.a));
+        for (int i = 0; i < this.a.length-1; i++) {
             int min_index = i;
-            for (int j = i+1; j < a.length; j++) {
-                if(a[min_index]>a[j]){
+            for (int j = i+1; j < this.a.length; j++) {
+                if(this.a[min_index]>this.a[j]){
                     min_index = j;
                 }
             }
-            int temp = a[i];
-            a[i] = a[min_index];
-            a[min_index] = temp;
-            System.out.println(Arrays.toString(a));
+            int temp = this.a[i];
+            this.a[i] = this.a[min_index];
+            this.a[min_index] = temp;
+            System.out.println(Arrays.toString(this.a));
         }
     }
+
+
+}
+
+
+
+public class Demo1_sort {
+
 
     public static void main(String[] args) {
         // System.out.println("Hello world!");
         // int a[] = {10,9,8,7,6,5,4,3,2,1};
-        int a[] = {7,6,1,3,4,5,8,10,9,2};
+        int[] a = {7,6,1,3,4,5,8,10,9,2};
 
-        bubble_sort(a);
-        select_sort(a);
+        Sort s = new Sort();
+        s.select_sort(a);
+        s.bubble_sort(a);
+
     }
 
 
